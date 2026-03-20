e8e509f29a0417c10a9e50bb170933d2428d01ce
Denis Geidman <denis.geidman@datatailr.com>
2026-03-20 12:50:32 +0100

    rename

--- stock_price_processing/deploy.py
+++ stock_price_processing/deploy.py
@@ -6,21 +6,21 @@ import stock_price_processing.dashboard.monitoring as dashboard_entrypoint
 
 
 price_server = Service(
-    name="price_server",
+    name="Price Server",
     entrypoint=price_server_main,
     resources=Resources(memory="1g", cpu=1),
     python_requirements="stock_price_processing/requirements.txt",
 )
 
 price_processing = Service(
-    name="price_processing",
+    name="Price Processor",
     entrypoint=price_processing_main,
     resources=Resources(memory="2g", cpu=1),
     python_requirements="stock_price_processing/requirements.txt",
 )
 
 dashboard = App(
-    name="Exchange Monitor",
+    name="Price Server Dashboard",
     entrypoint=dashboard_entrypoint,
     framework="flask",
     resources=Resources(memory="1g", cpu=1),
